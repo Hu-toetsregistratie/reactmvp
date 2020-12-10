3e7060d5b2b7fcab985bf269e0c2cdd456d073db
Nikki <nikki.westenborg@student.hu.nl>
2020-12-10 12:16:51 +0100

    mergefix

---
+++ src/Components/resultaten.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+//import '/src/App.css';
+
+function Resultaten() {
+    return (
+        <div className='content'>
+            <h1>Resultaten overzicht</h1>
+        </div>
+    );
+}
+
+export default withRouter(Resultaten);
