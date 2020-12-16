5840938784e3117c05b70cc9886d31f269cfe1a5
Sybren hijmans <sybrenhijmans@gmail.com>
2020-12-16 12:04:24 +0100

    Overzicht Studentindividueel gefiksed, met data vanuit fetch (Met resultaten van onvoldoende of voldoende) Naamgeving en container file aangemaakt

--- src/Routes.js
+++ src/Routes.js
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom'
 import {GradesView} from "./Containers/GradesView";
 import {StudentsView} from "./Containers/StudentsView"
 import {BarChartView} from "./Containers/BarChartView"
-import {StudentIndividual} from "./Components/StudentTable(individual)";
+import {IndividualStudentView} from "./Containers/IndividualStudentView";
 
 
 const BaseRouter = () => (
@@ -12,7 +12,7 @@ const BaseRouter = () => (
     <Route Exact path='/GradesView' component={GradesView} />
     <Route Exact path='/StudentsView' component={StudentsView} />
     <Route Exact path='/BarChartView' component={BarChartView} />
-    <Route Exact='/StudentIndividual' component={StudentIndividual}/>
+    <Route Exact='/StudentIndividual' component={IndividualStudentView}/>
 </div>
 
 );
